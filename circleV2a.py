e99370347c6d43c4788d8e7760a60499949d94ba
Nicole Sim <nicolesim@Nicoles-MacBook-Air.local>
2021-09-16 14:15:24 -0400

    Initial commit

---
+++ circleV2a.py
@@ -0,0 +1,8 @@
+#circleV2a
+#This version will accept a user input
+PI=3.14
+radius_str=input("enter a value for the radius:")
+radius=float(radius_str)
+circumference=2*PI*radius
+area=PI*radius*radius
+print("Circumference=", circumference, "area=", area)
---
+++ cirlceV1a.py
@@ -0,0 +1,11 @@
+#circleV1a by Nicole
+#Calculate the area and circumference of a circle with a provided radius from the program
+radius=3.5
+PI=3.14
+circumference=0.0
+area=0.0
+
+circumference=2 * PI * radius
+area=PI * radius * radius
+
+print("circumference=", circumference, "\narea=", area)
